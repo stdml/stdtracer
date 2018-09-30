7fb574d78ba86b445cc9dd94ef953922bcac286f
lg <lg4869@outlook.com>
2018-09-30 13:42:37 +0800

    use gtest

--- tests/test_1.cpp
+++ tests/test_1.cpp
@@ -1,5 +1,7 @@
 #include <stdtracer>
 
+#include <gtest/gtest.h>
+
 void f() { TRACE(__func__); }
 
 void g()
@@ -8,9 +10,14 @@ void g()
     for (int i = 0; i < 10; ++i) { f(); }
 }
 
-int main()
+TEST(suite, test1)
 {
     TRACE(__func__);
     g();
-    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
