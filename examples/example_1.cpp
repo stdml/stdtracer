8bc44ec4367e10264febf2d9b29c9b5de5de244f
lg <lg4869@outlook.com>
2018-09-30 14:46:58 +0800

    update example

--- examples/example_1.cpp
+++ examples/example_1.cpp
@@ -1,11 +1,23 @@
 #include <stdtracer>
 
-void f() { TRACE(__func__); }
+int fibo(int n)
+{
+    // don't use in recursive function!
+    if (n < 2) { return 1; }
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+void f(int n)
+{
+    TRACE(__func__);
+    int x = fibo(n);
+    printf("fibo(%d) = %d\n", n, x);
+}
 
 void g()
 {
     TRACE(__func__);
-    for (int i = 0; i < 10; ++i) { f(); }
+    for (int i = 0; i < 43; ++i) { f(i); }
 }
 
 int main()
