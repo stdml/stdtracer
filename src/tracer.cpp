9a931b5e7c429b0e153226404d40a6450b2eaea0
lg <lg4869@outlook.com>
2019-08-01 15:17:25 +0800

    add a new macro: DEFINE_TRACE_CONTEXTS

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -1,6 +1,3 @@
 #include <stdtracer>
 
-stack_tracer_ctx_t default_stack_tracer_ctx("global");
-
-simple_tracer_ctx_t default_simple_ctx("global");
-log_tracer_ctx_t default_log_ctx("global");
+DEFINE_TRACE_CONTEXTS;
