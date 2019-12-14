5c2b659ebc5daa2d6d8af01165157ea99d809f8e
lg <lg4869@outlook.com>
2019-12-14 12:13:04 +0000

    link thread

--- cmake/examples.cmake
+++ cmake/examples.cmake
@@ -1,22 +1,23 @@
+FIND_PACKAGE(Threads)
+
 FUNCTION(ADD_TRACE_EXAMPLE target)
     ADD_EXECUTABLE(${target} ${ARGN})
     TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${CMAKE_SOURCE_DIR}/include)
     TARGET_LINK_LIBRARIES(${target} stdtracer)
+    TARGET_LINK_LIBRARIES(${target} Threads::Threads)
 ENDFUNCTION()
 
 FUNCTION(ADD_TRACE_DISABLED_EXAMPLE target)
     ADD_EXECUTABLE(${target} ${ARGN})
     TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${CMAKE_SOURCE_DIR}/include)
     TARGET_COMPILE_DEFINITIONS(${target} PRIVATE -DSTD_TRACER_DISABLE)
+    TARGET_LINK_LIBRARIES(${target} Threads::Threads)
 ENDFUNCTION()
 
 FILE(GLOB examples examples/example_*.cpp)
 FOREACH(src ${examples})
     GET_FILENAME_COMPONENT(name ${src} NAME_WE)
-    STRING(REPLACE "_"
-                   "-"
-                   name
-                   ${name})
+    STRING(REPLACE "_" "-" name ${name})
     ADD_TRACE_EXAMPLE(${name} ${src})
     ADD_TRACE_DISABLED_EXAMPLE(${name}-disabled ${src})
 ENDFOREACH()
