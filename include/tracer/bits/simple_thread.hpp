0793be4d83349a85d2c1928b6f31797ebc240cfa
lg <lg4869@outlook.com>
2020-02-09 16:06:59 +0000

    new include :: <tracer/simple_thread>

---
+++ include/tracer/bits/simple_thread.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <bits/stdtracer_ctx_thread.hpp>
+#include <bits/stdtracer_scope.hpp>
+
+// BEGIN defaults
+using default_clock_t = std::chrono::high_resolution_clock;
+using default_duration_t = std::chrono::duration<double>;
+
+using thread_tracer_ctx_t =
+    thread_tracer_ctx_t_<default_clock_t, default_duration_t>;
+// END defaults
+
+using tracer_t = scope_t_<thread_tracer_ctx_t>;
+
+extern thread_tracer_ctx_t default_thread_tracer_ctx;
+
+#define TRACE_SCOPE(name) tracer_t _((name), default_thread_tracer_ctx)
+
+#define TRACE_STMT(e)                                                          \
+    {                                                                          \
+        tracer_t _(#e, default_thread_tracer_ctx);                             \
+        e;                                                                     \
+    }
+
+#define TRACE_EXPR(e)                                                          \
+    [&]() {                                                                    \
+        tracer_t _(#e, default_thread_tracer_ctx);                             \
+        return (e);                                                            \
+    }()
