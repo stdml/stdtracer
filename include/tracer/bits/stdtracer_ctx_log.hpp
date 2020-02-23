531c49a6930822050a9593253ffea108acaa2290
lg <lg4869@outlook.com>
2020-02-23 22:06:31 +0000

    snprintf

--- include/tracer/bits/stdtracer_ctx_log.hpp
+++ include/tracer/bits/stdtracer_ctx_log.hpp
@@ -12,6 +12,12 @@
 template <typename clock_t, typename duration_t>
 class log_tracer_ctx_t_
 {
+    const std::string name;
+    const std::chrono::time_point<clock_t> t0;
+
+    int depth;
+    std::deque<FILE *> log_files;
+
   public:
     explicit log_tracer_ctx_t_(const std::string &name)
         : name(name), t0(clock_t::now()), depth(0)
@@ -19,12 +25,10 @@ class log_tracer_ctx_t_
         log_files.push_front(stdout);
     }
 
-    ~log_tracer_ctx_t_() {}
-
     void in(const std::string &name)
     {
         indent();
-        WITH_XTERM(1, 35, printf("{ // [%s]", name.c_str()));
+        WITH_XTERM(1, 35, std::printf("{ // [%s]", name.c_str()));
         putchar('\n');
         ++depth;
     }
@@ -32,32 +36,32 @@ class log_tracer_ctx_t_
     void out(const std::string &name, const duration_t &d)
     {
         --depth;
-
-        char buffer[128];
-        sprintf(buffer, "[%s] took ", name.c_str());
+        char buffer[512];
+        std::snprintf(buffer, 400, "[%s", name.c_str());
+        char *p = buffer + std::strlen(buffer);
         [&](double t) {
             if (t < 1) {
-                sprintf(buffer + strlen(buffer), "%.2fms", t * 1000);
+                std::sprintf(p, "] took %.2fms", t * 1000);
             } else {
-                sprintf(buffer + strlen(buffer), "%.2fs", t);
+                std::sprintf(p, "] took %.2fs", t);
             }
         }(d.count());
         indent();
-        WITH_XTERM(1, 32, printf("} // %s", buffer));
-        putchar('\n');
+        WITH_XTERM(1, 32, std::printf("} // %s", buffer));
+        std::putchar('\n');
     }
 
     void indent(FILE *fp = stdout)
     {
-        for (int i = 0; i < depth; ++i) { fprintf(fp, "    "); }
+        for (int i = 0; i < depth; ++i) { std::fprintf(fp, "    "); }
     }
 
     template <typename... Args>
     void logf1(FILE *fp, const Args &... args)
     {
-        fprintf(fp, "// ");
-        fprintf(fp, args...);
-        fputc('\n', fp);
+        std::fprintf(fp, "// ");
+        std::fprintf(fp, args...);
+        std::fputc('\n', fp);
     }
 
     template <typename... Args>
@@ -69,12 +73,4 @@ class log_tracer_ctx_t_
             break;  // only log to the first
         }
     }
-
-    std::deque<FILE *> log_files;
-
-  private:
-    const std::string name;
-    const std::chrono::time_point<clock_t> t0;
-
-    int depth;
 };
