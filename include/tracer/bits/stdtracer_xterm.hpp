3426d52a87ea7e35495401cce777147a2eefe69e
lg <lg4869@outlook.com>
2020-05-19 10:15:08 +0100

    -DHAVE_UNISTD IF(UNIX) fixes(#18)

--- include/tracer/bits/stdtracer_xterm.hpp
+++ include/tracer/bits/stdtracer_xterm.hpp
@@ -1,19 +1,25 @@
 #pragma once
 #include <cstdio>
 
-#include <unistd.h>
+#ifdef HAVE_UNISTD
+#    include <unistd.h>
+
+inline bool std_isatty(FILE *fp) { return isatty(fileno(fp)); }
+#else
+inline bool std_isatty(FILE *fp) { return false; }
+#endif
 
 struct xterm_t {
     const bool is_tty;
 
-    xterm_t(uint8_t b, uint8_t f) : is_tty(isatty(fileno(stdout)))
+    xterm_t(uint8_t b, uint8_t f) : is_tty(std_isatty(stdout))
     {
-        if (is_tty) { std::printf("\e[%u;%um", b, f); }
+        if (is_tty) { std::fprintf(stdout, "\e[%u;%um", b, f); }
     }
 
     ~xterm_t()
     {
-        if (is_tty) { std::printf("\e[m"); }
+        if (is_tty) { std::fprintf(stdout, "\e[m"); }
     }
 };
 
