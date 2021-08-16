977b4c96ffb863610ca0ac910cfb60f1098e2ec9
lg <lg4869@outlook.com>
2021-08-16 11:55:37 +0100

    <tracer/rate>

---
+++ include/tracer/bits/rate.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <chrono>
+#include <cstring>
+#include <string>
+
+#include <tracer/bits/stdtracer_base.hpp>
+#include <tracer/bits/stdtracer_xterm.hpp>
+
+template <typename D, typename N>
+class rate_reporter_t_
+{
+    const std::string name_;
+    const std::string unit_;
+    FILE *fp_;
+
+  public:
+    explicit rate_reporter_t_(std::string name, std::string unit,
+                              FILE *fp = stderr)
+        : name_(std::move(name)), unit_(std::move(unit)), fp_(fp)
+    {
+    }
+
+    void operator()(D d, N n, const char *prefix) const
+    {
+        fprintf(fp_, "%s%.2f %s/sec | %s\n", prefix, (double)n / d.count(),
+                unit_.c_str(), name_.c_str());
+    }
+
+    void operator()(D d, N n) const { (*this)(d, n, "%%%% "); }
+};
+
+class rate_ctx_t_
+{
+    using D = std::chrono::duration<double>;
+    using N = int64_t;
+    using reporter_t = rate_reporter_t_<D, N>;
+
+    reporter_t reporter_;
+
+    N amount_;
+    D duration_;
+    FILE *fp_;
+
+  public:
+    explicit rate_ctx_t_(std::string name, std::string unit, FILE *fp = stderr)
+        : reporter_(std::move(name), std::move(unit), fp), fp_(fp)
+    {
+    }
+
+    ~rate_ctx_t_() { reporter_(duration_, amount_, "[overall rate]: "); }
+
+    void in() {}
+
+    void out(const D &d, int64_t n)
+    {
+        reporter_(d, n, "[rate]: ");
+        amount_ += n;
+        duration_ += d;
+    }
+};
+
+class rate_scope_t_
+{
+    rate_ctx_t_ &ctx_;
+    using N = int64_t;
+
+    using C = std::chrono::high_resolution_clock;
+    using T = std::chrono::time_point<C>;
+
+    T t0;
+    N n;
+
+  public:
+    rate_scope_t_(rate_ctx_t_ &ctx, N n) : ctx_(ctx), t0(C::now()), n(n)
+    {
+        ctx_.in();
+    }
+
+    ~rate_scope_t_() { ctx_.out(C::now() - t0, n); }
+};
