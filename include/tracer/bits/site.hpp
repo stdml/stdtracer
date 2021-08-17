c763e4bc3053c56c6daef260e982e7fc0bb7ce33
lg <lg4869@outlook.com>
2021-08-17 10:42:44 +0000

    u

--- include/tracer/bits/site.hpp
+++ include/tracer/bits/site.hpp
@@ -7,12 +7,38 @@
 #include <tracer/bits/config.hpp>
 #include <tracer/bits/stdtracer_scope.hpp>
 
-template <typename clock_t, typename duration_t>
-class simple_site_ctx_t_
+class site_reporter_
 {
     const std::string filename_;
     const std::string name_;
     const int lino_;
+
+    FILE *fp_;
+    bool report_;
+
+  public:
+    site_reporter_(std::string filename, int lino, std::string name)
+        : filename_(std::move(filename)), lino_(lino), name_(std::move(name)),
+          fp_(stderr), report_(report_stdout())
+    {
+    }
+
+    template <typename D, typename N>
+    void operator()(D d, N n) const
+    {
+        if (!report_) { return; }
+        fprintf(fp_,
+                "%%%% site called %8" PRId32
+                " times, mean: %8.3fms, total: %8.3fs, file://%s:%d : %s\n",
+                n, d.count() * 1e3 / n, d.count(), filename_.c_str(), lino_,
+                name_.c_str());
+    }
+};
+
+template <typename clock_t, typename duration_t>
+class simple_site_ctx_t_
+{
+    site_reporter_ report;
 
     using N = int32_t;
 
@@ -21,18 +47,11 @@ class simple_site_ctx_t_
 
   public:
     simple_site_ctx_t_(std::string filename, int lino, std::string name)
-        : filename_(std::move(filename)), lino_(lino), name_(std::move(name))
+        : report(std::move(filename), lino, std::move(name))
     {
     }
 
-    ~simple_site_ctx_t_()
-    {
-        fprintf(stderr,
-                "%%%% site called %8" PRId32
-                " times, mean: %8.3fms, total: %8.3fs, file://%s:%d : %s\n",
-                count_, duration_.count() * 1e3 / count_, duration_.count(),
-                filename_.c_str(), lino_, name_.c_str());
-    }
+    ~simple_site_ctx_t_() { report(duration_, count_); }
 
     void in()
     {
---
+++ tests/test_site.cpp
@@ -0,0 +1,9 @@
+#include "testing.hpp"
+
+#include <tracer/site>
+
+void f()
+{  // TODO
+}
+
+TEST(suite, test_1) { f(); }
