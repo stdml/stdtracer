d9d046324d399e19eec42fd26a4d7432e308221b
lg <lg4869@outlook.com>
2021-08-20 21:49:06 +0100

    test

--- tests/test_patient.cpp
+++ tests/test_patient.cpp
@@ -5,12 +5,18 @@
 int f()
 {
     WITH_PATIENT(__func__);
+    for (int i=0; i < 10; ++i) {
+        YIELD_PATIENT("forloop");
+    }
     return 1;
 }
 
 int g()
 {
     WITH_PATIENT(__func__, 1);
+    for (int i=0; i < 10; ++i) {
+        YIELD_PATIENT_("forloop: %d", i);
+    }
     return 1;
 }
 
