c63b93a971fd9eb6eb647819d5a9330c5351df26
lg <lg4869@outlook.com>
2024-03-29 11:39:00 +0000

    build deb

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -1,4 +1,15 @@
-INCLUDE(CPack)
-
 SET(CPACK_GENERATOR "TGZ")
 SET(CPACK_PACKAGE_VERSION "latest")
+
+IF(BUILD_DEB)
+    SET(CPACK_GENERATOR "TGZ;DEB")
+    SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "root@localhost")
+    SET(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+    SET(CPACK_PACKAGE_VERSION "0.0.0")
+ENDIF()
+
+# CPackDeb: Debian package versioning ([<epoch>:]<version>[-<release>])
+# should confirm to "^([0-9]+:)?[0-9][A-Za-z0-9.+~-]*$" regex in order to
+
+
+INCLUDE(CPack)
